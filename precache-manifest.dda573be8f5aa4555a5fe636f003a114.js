0ac5cc5e666a912b130c04846846eb8593ced899
Yen Vu Thi <vu.thi.yen@sun-asterisk.com>
2020-05-21 10:46:39 +0700

    Updates

---
+++ precache-manifest.dda573be8f5aa4555a5fe636f003a114.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b9edfe1774cc9446ec65bc28470b6bb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bc6820a77820ede61163",
+    "url": "./static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "548cf1bd14be04425ec9",
+    "url": "./static/js/2.b971a2b1.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "./static/js/2.b971a2b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc6820a77820ede61163",
+    "url": "./static/js/main.3b1c825d.chunk.js"
+  },
+  {
+    "revision": "44b6d3f5efb3e0f9df7f",
+    "url": "./static/js/runtime-main.79237f47.js"
+  }
+]);
